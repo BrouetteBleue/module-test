650da9276031e6b923b57825297cf619920b23e6
Samy Kebli <jaivendutonchat@hotmail.com>
2023-03-20 15:58:32 +0100

    tests user

--- src/user.php
+++ src/user.php
@@ -12,6 +12,36 @@ class User {
         $this->prenom = $prenom;
         $this->age = $age;
     }
+
+    // getters
+    public function getEmail() {
+        return $this->email;
+    }
+    public function getNom() {
+        return $this->nom;
+    }
+    public function getPrenom() {
+        return $this->prenom;
+    }
+    public function getAge() {
+        return $this->age;
+    }
+
+    // setters
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+    public function setNom($nom) {
+        $this->nom = $nom;
+    }
+    public function setPrenom($prenom) {
+        $this->prenom = $prenom;
+    }
+    public function setAge($age) {
+        $this->age = $age;
+    }
+
+    
     
    public function isValid() {
         if (!$this->email || !filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
--- tests/userTest.php
+++ tests/userTest.php
@@ -29,5 +29,38 @@ class UserTest extends TestCase {
         $user6 = new User("jane.smith@example.com", "Smith", "Jane", 10);
         $this->assertFalse($user6->isValid());
     }
+
+    public function testEmail(){
+        $email1 = new User("john.doe@example.fr", "Doe", "John", 30);
+        // test if email is valid
+        $this->assertEquals($email1->getEmail(),filter_var($email1->getEmail(), FILTER_VALIDATE_EMAIL));
+
+    }
+
+    public function testNom(){
+        $user = new User("john.doe@example.com", "Doe", "John", 25);
+        $this->assertIsString($user->getNom());
+
+    }
+
+    public function testPrenom() {
+        $user = new User("john.doe@example.com", "Doe", "45John", 25);
+        $this->assertIsString($user->getPrenom());
+    }
+
+    public function testAgeIsInt() {
+        $user = new User("john.doe@example.com", "Doe", "John", 25);
+        $this->assertIsInt($user->getAge());
+    }
+
+    public function testNomSansChiffres() {
+        $user = new User("john.doe@example.com", "Doe", "John", 25);
+        $this->assertMatchesRegularExpression('/^[^\d]*$/', $user->getNom());
+    }
+
+    public function testPrenomSansChiffres() {
+        $user = new User("john.doe@example.com", "Do4e", "Joh-n", 25);
+        $this->assertMatchesRegularExpression('/^[^\d]*$/', $user->getPrenom());
+    }
     
 }
